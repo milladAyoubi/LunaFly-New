dd56423a6883de398cb672fe82f0be74e608e53b
Millad Ayoubi <Milladayoubi@gmail.com>
2023-01-29 22:17:02 -0500

    LunaFly Project

---
+++ index.js
@@ -0,0 +1,85 @@
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '60px',
+    duration: 1500,
+    delay: 400,
+
+
+})
+
+
+sr.reveal('.mainText', {
+    delay: 200,
+    distance: '60px',
+    duration: 2400,
+    origin: 'left',
+
+})
+
+
+sr.reveal('.Destinations', {
+    delay: 200,
+    distance: '60px',
+    duration: 2400,
+    origin: 'bottom',
+
+})
+
+
+sr.reveal('.about_img', {
+    delay: 200,
+    distance: '60px',
+    duration: 2000,
+    origin: 'left',
+
+})
+
+
+sr.reveal('.about_text', {
+    delay: 1000,
+    distance: '60px',
+    duration: 1800,
+    origin: 'left',
+
+})
+
+
+sr.reveal('.gallery__item:nth-child(1) ', {
+    delay: 200,
+    distance: '60px',
+    duration: 1800,
+    origin: 'left',
+
+})
+
+sr.reveal('.gallery__item:nth-child(2) ', {
+    delay: 800,
+    distance: '60px',
+    duration: 1800,
+    origin: 'left',
+
+})
+sr.reveal('.gallery__item:nth-child(3) ', {
+    delay: 1000,
+    distance: '60px',
+    duration: 1800,
+    origin: 'left',
+
+})
+
+sr.reveal('.gallery__item:nth-child(4) ', {
+    delay: 1200,
+    distance: '60px',
+    duration: 1800,
+    origin: 'left',
+
+})
+
+
+sr.reveal('.footerText', {
+    delay: 200,
+    distance: '60px',
+    duration: 1800,
+    origin: 'bottom',
+
+})
